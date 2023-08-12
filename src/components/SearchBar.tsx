371e2bf17010657bf4fdb081dac096a31d833ce0
nutaptap <nutaptap@gmail.com>
2023-08-12 13:24:55 +0200

    Fixed names for the svg properties

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -37,9 +37,9 @@ export function SearchBar({ onSearchSubmit }: SearchBarProps) {
           viewBox="0 0 24 24"
           fill="none"
           stroke="#5C82F2"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <title>Search</title>
           <circle cx="11" cy="11" r="8"></circle>
