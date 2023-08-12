c016e71b148f0f4defc3040dfac36c35b0f0ff76
nutaptap <nutaptap@gmail.com>
2023-08-12 10:48:04 +0200

    Added type interfaces for the pokemon data

---
+++ src/types.ts
@@ -0,0 +1,53 @@
+export interface Ability {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
+
+export interface Type {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface OfficialArtwork {
+  front_default: string;
+  front_shiny: string;
+}
+
+export interface Stat {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface Pokemon {
+  abilities: Ability[];
+  base_experience: number;
+  forms: any[];
+  game_indices: any[];
+  height: number;
+  held_items: any[];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: any[];
+  name: string;
+  order: number;
+  past_types: any[];
+  species: any;
+  sprites: {
+    other: { "official-artwork": OfficialArtwork };
+  };
+  stats: Stat[];
+  types: Type[];
+  weight: number;
+}
