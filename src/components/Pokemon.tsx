717cae2388a224d3ecc3ad95cb001ce90f13e9b7
nutaptap <nutaptap@gmail.com>
2023-08-13 10:37:34 +0200

    Fixed html semantics in the table

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -23,67 +23,73 @@ export function Pokemon({ pokemonData }: PokemonProps) {
       </section>
       <section>
         <table>
-          <tr>
-            <td>Height</td>
-            <td>{pokemonData && formatHeight(pokemonData.height)}</td>
-          </tr>
-          <tr>
-            <td>Weight</td>
-            <td>{pokemonData && formatWeight(pokemonData.weight)}</td>
-          </tr>
-          <tr>
-            <td>Abilities</td>
-            <td>
-              {pokemonData?.abilities[0] && (
-                <span className={`ability ${pokemonData.types[0].type.name}`}>
-                  {pokemonData.abilities[0].ability.name.toUpperCase()}
-                </span>
-              )}
-              {pokemonData?.abilities[1] && (
-                <span className={`ability ${pokemonData.types[0].type.name}`}>
-                  {pokemonData.abilities[1].ability.name.toUpperCase()}
-                </span>
-              )}
-            </td>
-          </tr>
-          <tr>
-            <td>Type</td>
-            <td>
-              {pokemonData?.types[0] && (
-                <span className={`type ${pokemonData.types[0].type.name}`}>
-                  <span>{pokemonData.types[0].type.name} </span>
-                  <img
-                    className="icon"
-                    src={`/${pokemonData.types[0].type.name}.svg`}
-                  />
-                </span>
-              )}
-              {pokemonData?.types[1] && (
-                <span className={`type ${pokemonData.types[1].type.name}`}>
-                  <span>{pokemonData.types[1].type.name}</span>
-                  <img
-                    className="icon"
-                    src={`/${pokemonData.types[1].type.name}.svg`}
-                  />
-                </span>
-              )}
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Height</td>
+              <td>{pokemonData && formatHeight(pokemonData.height)}</td>
+            </tr>
+            <tr>
+              <td>Weight</td>
+              <td>{pokemonData && formatWeight(pokemonData.weight)}</td>
+            </tr>
+            <tr>
+              <td>Abilities</td>
+              <td>
+                {pokemonData?.abilities[0] && (
+                  <span className={`ability ${pokemonData.types[0].type.name}`}>
+                    {pokemonData.abilities[0].ability.name.toUpperCase()}
+                  </span>
+                )}
+                {pokemonData?.abilities[1] && (
+                  <span className={`ability ${pokemonData.types[0].type.name}`}>
+                    {pokemonData.abilities[1].ability.name.toUpperCase()}
+                  </span>
+                )}
+              </td>
+            </tr>
+            <tr>
+              <td>Type</td>
+              <td>
+                {pokemonData?.types[0] && (
+                  <span className={`type ${pokemonData.types[0].type.name}`}>
+                    <span>{pokemonData.types[0].type.name} </span>
+                    <img
+                      className="icon"
+                      src={`/${pokemonData.types[0].type.name}.svg`}
+                    />
+                  </span>
+                )}
+                {pokemonData?.types[1] && (
+                  <span className={`type ${pokemonData.types[1].type.name}`}>
+                    <span>{pokemonData.types[1].type.name}</span>
+                    <img
+                      className="icon"
+                      src={`/${pokemonData.types[1].type.name}.svg`}
+                    />
+                  </span>
+                )}
+              </td>
+            </tr>
+          </tbody>
         </table>
       </section>
       <section>
         <table>
-          {pokemonData?.stats.map((stat) => (
-            <tr className="stat" key={stat.stat.name}>
-              <td>{stat.stat.name}</td>
-              <td>{stat.base_stat}</td>
-              <ProgressBar
-                currentValue={stat.base_stat}
-                maxValue={findMaxValue(pokemonData.stats)}
-                color={pokemonData.types[0].type.name}
-              />
-            </tr>
-          ))}
+          <tbody>
+            {pokemonData?.stats.map((stat) => (
+              <tr className="stat" key={stat.stat.name}>
+                <td>{stat.stat.name}</td>
+                <td>{stat.base_stat}</td>
+                <td>
+                  <ProgressBar
+                    currentValue={stat.base_stat}
+                    maxValue={findMaxValue(pokemonData.stats)}
+                    color={pokemonData.types[0].type.name}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </section>
     </article>
